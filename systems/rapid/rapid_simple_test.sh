5bd287301dc0789b85f7cf7ba2a8add61e01107b
Anh Le Tuan <anh.letuan@tu-berlin.de>
2024-11-12 14:48:58 +0000

    update rapid

--- systems/rapid/rapid_simple_test.sh
+++ systems/rapid/rapid_simple_test.sh
@@ -1,20 +1,33 @@
+#!/bin/bash
+
+# Set the scaling factor
 SF=$1
-python3 data/ldbc_snb/ldbc_snb_to_graph.py $SF
-bash data/ldbc_snb/refine_graph.sh $SF
+
+# Run initial Python scripts and refinement bash script
+python3 data/ldbc_snb/ldbc_snb_to_graph.py "$SF"
+bash data/ldbc_snb/refine_graph.sh "$SF"
 python3 data/ldbc_snb/ldbc_snb_query_to_graph.py 
 
+# Replace '.' with '_' in scaling factor for filenames
 SF=$(echo "$SF" | sed 's/\./_/g')
+
+# Define paths for the graph files
 GRAPH_BASE="data/ldbc_snb/static_id_snb_${SF}.g"
-echo 
 GRAPH_INSE="data/ldbc_snb/dynamic_id_snb_${SF}.g"
-GRAPH_QUERY='data/ldbc_snb/queries/snb_ic01.g'
 
-./RapidFlow/build/streaming/RapidFlow.out -d ${GRAPH_BASE} -u ${GRAPH_INSE} -q ${GRAPH_QUERY}
-echo
+# Define the list of query files
+queries=(
+    "snb_c_triangle.g"
+    "snb_c_3clique.g"
+    "snb_c_4clique.g"
+    "snb_c_diamon.g"
+    "snb_c_dumbell.g"
+    "snb_c_retangular.g"
+)
 
-
-
-
-
-
-
+# Loop through each query and run the RapidFlow command
+for query in "${queries[@]}"; do
+    GRAPH_QUERY="data/ldbc_snb/queries/${query}"
+    ./RapidFlow/build/streaming/RapidFlow.out -d "${GRAPH_BASE}" -u "${GRAPH_INSE}" -q "${GRAPH_QUERY}"
+    echo
+done
