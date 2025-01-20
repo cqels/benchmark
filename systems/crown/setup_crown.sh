4b53fc8941e7774ed3f2e8d6be30c10067165f13
Anh Le Tuan <anh.letuan@tu-berlin.de>
2025-01-20 12:30:19 +0100

    update crown

--- systems/crown/setup_crown.sh
+++ systems/crown/setup_crown.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
+
 SCRIPT_PATH=$(dirname $(readlink -f $0))
+cd $SCRIPT_PATH
 
 REPO_URL="https://github.com/hkustDB/CROWN.git"
 
 TARGET_DIR=$(basename "${REPO_URL}" .git)
-
-echo $TARGET_DIR
-echo $SCRIPT_PATH
 
 if [ ! -d "$SCRIPT_PATH/$TARGET_DIR" ]; then
     git clone "$REPO_URL" "$SCRIPT_PATH/$TARGET_DIR"
@@ -13,4 +13,24 @@ else
     echo "Directory $TARGET_DIR already exists. Skipping clone."
 fi
 
+# Check if a container named "crown" exists
+container_id=$(docker ps -aq --filter "name=crown")
 
+if [ -n "$container_id" ]; then
+    echo "Container 'crown' exists. Removing it..."
+    # Remove the container
+    docker rm -f crown
+    echo "Container 'crown' has been removed."
+else
+    echo "No container named 'crown' exists."
+fi
+
+
+# Build the Docker image
+docker build -f Dockerfile.crown -t crown_image .
+
+# Run the container with a valid default command
+docker run -di --name crown -v "$(pwd):/root" crown_image 
+
+# Optional: Attach to the container
+# docker exec -it crown bash
